206c0132aa06145406b3ce97e2ea25960906d44a
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2026-02-13 15:43:28 -0800

    Updated gh-pages 39876VER

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-46303f49.js',
+                path_to_root + 'searchindex-ba768e87.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
